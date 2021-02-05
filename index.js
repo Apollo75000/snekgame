d7ab42383fae3563cc0f56f213f4ecb4bbef44d8
Apollo75000 <78597084+Apollo75000@users.noreply.github.com>
2021-02-05 19:24:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-URL //127.0.0.1:5500/index.html = new URL("http://www.snekgame.com/");
+
 
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
@@ -45,14 +45,26 @@ function drawGame(){
 
     drawScore();
 
-    if(score > 2){
-        speed = 11;
-    }
     if(score > 5){
-        speed = 10;
+        speed = 9;
     }
     if(score > 10){
-        speed = 15;
+        speed = 10;
+    }
+    if(score > 15){
+        speed = 11
+    }
+    if(score > 20){
+        speed = 13;
+    }
+    if(score > 25){
+        speed = 15
+    }
+    if(score > 30){
+        speed = 16;
+    }
+    if(score > 40){
+        speed = 18;
     }
 
     setTimeout(drawGame, 1000/ speed);
@@ -166,7 +178,7 @@ document.body.addEventListener('keydown', keyDown);
 
 function keyDown(event){
     //up
-    if(event.keyCode == 87){
+    if(event.keyCode == 38){
         if(yVelocity == 1)
             return;
         yVelocity = -1;
@@ -174,7 +186,7 @@ function keyDown(event){
     }
 
     //down
-    if(event.keyCode == 83){
+    if(event.keyCode == 40){
         if(yVelocity == -1)
             return;
         yVelocity = 1;
@@ -182,7 +194,7 @@ function keyDown(event){
     }
 
     //left
-    if(event.keyCode == 65){
+    if(event.keyCode == 37){
         if(xVelocity == 1)
             return;
         yVelocity = 0;
@@ -190,7 +202,7 @@ function keyDown(event){
     }
 
      //right
-     if(event.keyCode == 68){
+     if(event.keyCode == 39){
         if(xVelocity == -1)
         return;
         yVelocity = 0;
@@ -199,3 +211,4 @@ function keyDown(event){
 }
 
 drawGame();
+
