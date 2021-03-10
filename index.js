324e7ba268d1f86e45864d4d0da00bf0aff3b66b
Apollo75000 <78597084+Apollo75000@users.noreply.github.com>
2021-03-10 15:05:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ document.body.addEventListener('keydown', keyDown);
 
 function keyDown(event){
     //up
-    if(event.keyCode == 87){
+    if(event.keyCode == 38){
         if(yVelocity == 1)
             return;
         yVelocity = -1;
@@ -187,7 +187,7 @@ function keyDown(event){
     }
 
     //down
-    if(event.keyCode == 83){
+    if(event.keyCode == 40){
         if(yVelocity == -1)
             return;
         yVelocity = 1;
@@ -195,7 +195,7 @@ function keyDown(event){
     }
 
     //left
-    if(event.keyCode == 65){
+    if(event.keyCode == 37){
         if(xVelocity == 1)
             return;
         yVelocity = 0;
@@ -203,7 +203,7 @@ function keyDown(event){
     }
 
      //right
-     if(event.keyCode == 68){
+     if(event.keyCode == 39){
         if(xVelocity == -1)
         return;
         yVelocity = 0;
